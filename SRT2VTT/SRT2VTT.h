fa39d20b13989878334d216779823391d257c3d1
aggelos81 <aggelosred7@gmail.com>
2016-08-03 22:07:26 +0300

    Initial Commit

---
+++ SRT2VTT/SRT2VTT.h
@@ -0,0 +1,19 @@
+//
+//  SRT2VTT.h
+//  SRT2VTT
+//
+//  Created by Aggelos Papageorgiou on 03/08/16.
+//  Copyright © 2016 Aggelos Papageorgiou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SRT2VTT.
+FOUNDATION_EXPORT double SRT2VTTVersionNumber;
+
+//! Project version string for SRT2VTT.
+FOUNDATION_EXPORT const unsigned char SRT2VTTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SRT2VTT/PublicHeader.h>
+
+
